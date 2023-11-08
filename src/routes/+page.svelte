24f0ad57cb955662c3c90e3a251765a8b0455f2a
Brendan Malycky <bmalycky@hotmail.com>
2023-11-08 13:28:48 -0700

    created object array, rendered object data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  const objArr = [
+    {fName: "Tom", lName: "Riddle", house: "Slytherin"},
+    {fName: "Harry", lName: "Potter", house: "Gryffindor"},
+    {fName: "Hermione", lName: "Granger", house: "Gryffindor"},
+    {fName: "Cedric", lName: "Diggory", house: "Ravenclaw"},
+    {fName: "Draco", lName: "Malfoy", house: "Slytherin"}, 
+  ]
+</script>
+
+<main class="flex flex-wrap justify-center gap-10 p-5">
+  {#each objArr as character}
+    <div>
+      <h2 class="text-2xl mb-3">{character.fName} {character.lName}</h2>
+      <p class="text-center">{character.house}</p>
+    </div>
+  {/each}
+</main>
+
