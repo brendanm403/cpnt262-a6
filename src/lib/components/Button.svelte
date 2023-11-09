0899e0510dca8d5616b0b01efcaf7ac4a9908923
Brendan Malycky <bmalycky@hotmail.com>
2023-11-08 20:39:57 -0700

    created functions and variables to get values and have access globally

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -3,8 +3,8 @@
   const dispatch = createEventDispatcher();
   // function dispatch //
   const defaultFn = function() {
-    dispatch("getUserInput", {})
+    dispatch("renderCharacter", {})
   }
 </script>
 
-<button class="block my-1 bg-purple-400 p-2 text-xs rounded-2xl hover:bg-purple-600 mx-auto active:bg-purple-900 active:text-white active:scale-90" on:click={defaultFn}>Submit</button>
+<button class="block mt-5 bg-purple-400 p-2 text-xs rounded-2xl hover:bg-purple-600 mx-auto active:bg-purple-900 active:text-white active:scale-90" on:click={defaultFn}>Submit</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,34 @@
     {fName: "Cedric", lName: "Diggory", house: "Ravenclaw"},
     {fName: "Draco", lName: "Malfoy", house: "Slytherin"}, 
   ]
+  let firstName;
+  let lastName;
+  let house;
   // makes a new object based on an existing object //
   // let inputObj = Object.create(objArr[0]);
   // inputObj.fName = "carl"
   // console.log(inputObj);
+
+  const displayUserInput = function() {
+    console.log(firstName);
+    console.log(lastName);
+    console.log(house);  
+  }
+
+  // store the value entered in first name input in global variable //
+  const getFirstName = function(event) {
+    firstName = event.target.value; 
+  }
+
+  // store the value entered in last name input in global variable //
+  const getLastName = function(event) {
+    lastName = event.target.value; 
+  }
+
+  // store the value entered in house input in global variable //
+  const getHouse = function(event) {
+    house = event.target.value; 
+  }
 </script>
 
 <main class="flex flex-wrap justify-center gap-10 p-5">
@@ -23,9 +47,10 @@
 </main>
 
 <label class="mt-12 w-56 mx-auto block text-center" for="fName">Character First Name</label>
-<input class="block bg-black text-green-200 border-2 rounded-lg border-green-400 mx-auto" type="text" id="fName"><Button />
+<input class="block bg-black text-green-200 border-2 rounded-lg border-green-400 mx-auto" type="text" id="fName" on:change={getFirstName}>
 <label class="mt-5 w-56 mx-auto block text-center" for="lName">Character Last Name</label>
-<input class="block bg-black text-green-200 border-2 rounded-lg border-green-400 mx-auto" type="text" id="lName">
+<input class="block bg-black text-green-200 border-2 rounded-lg border-green-400 mx-auto" type="text" id="lName" on:change={getLastName}>
 <label class="mt-5 w-56 mx-auto block text-center" for="house">Character House</label>
-<input class="block bg-black text-green-200 border-2 rounded-lg border-green-400 mx-auto" type="text" id="house">
+<input class="block bg-black text-green-200 border-2 rounded-lg border-green-400 mx-auto" type="text" id="house" on:change={getHouse}>
+<Button on:renderCharacter={displayUserInput}/>
 
