c6ffe84dac07062010310de1445c6d3a211b53e1
Brendan Malycky <bmalycky@hotmail.com>
2023-11-08 19:57:28 -0700

    created button component and dispatch function

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,10 @@
+<script>
+  import {createEventDispatcher} from "svelte";
+  const dispatch = createEventDispatcher();
+  // function dispatch //
+  const defaultFn = function() {
+    dispatch("getUserInput", {})
+  }
+</script>
+
+<button class="block my-1 bg-purple-400 p-2 text-xs rounded-2xl hover:bg-purple-600 mx-auto active:bg-purple-900 active:text-white active:scale-90" on:click={defaultFn}>Submit</button>
