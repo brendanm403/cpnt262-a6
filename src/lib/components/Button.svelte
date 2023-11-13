02674d161c42467d3bb4d6d2cc761f8a6f889452
Brendan Malycky <bmalycky@hotmail.com>
2023-11-13 14:57:09 -0700

    changed button colours

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -8,4 +8,4 @@
   export let buttonText = "button text"
 </script>
 
-<button class="block mt-5 bg-purple-400 p-2 text-xs rounded-2xl hover:bg-purple-600 mx-auto active:bg-purple-900 active:text-white active:scale-90" on:click={defaultFn}>{buttonText}</button>
+<button class="block mt-5 bg-purple-600 p-2 text-white text-xs rounded-2xl hover:bg-purple-700 mx-auto active:bg-purple-900 active:text-white active:scale-90" on:click={defaultFn}>{buttonText}</button>
